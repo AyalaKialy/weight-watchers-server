2ff4cd4a85bc008663881d0d92d72645598e76ea
rutyCohen <213235542@mby.co.il>
2022-07-21 13:52:51 +0300

    fix user testing

--- service.mongoose/__test__/user.test.js
+++ service.mongoose/__test__/user.test.js
@@ -1,29 +1,11 @@
-
+ 
 const userService = require('../user.service');
 const userModel = require('../../models/user.model');
 
 const { expect } = require('chai');
 const sinon = require('sinon');
 
-test('returns an user',async () => {
-    const userObject={
-        id:1,
-        firstName: 'Rachel',
-        lastName: 'Keller',
-        city:'Jerusalem',
-        street:'Arimon',
-        number:1,
-        phone:'String',
-        email: 'rk@example.com',
-        height:1.
-    }
-    const id=1;
-    sinon.stub(userModel, 'findOne').returns(userObject);
-    const returnedUser = await userService.getUserById(id);
-    expect(returnedUser).equal(userObject);
-}),
-
-test('returns an user',async () => {
+describe('test users functionality', () => { 
     const usersObject=[{
         id:1,
         firstName: 'Rachel',
@@ -46,7 +28,16 @@ test('returns an user',async () => {
         email: 'kk@example.com',
         height:1.
     }]
-    sinon.stub(userModel, 'find').returns(usersObject);
-    const returnedUsers = await userService.getAllUsers();
-    expect(typeof returnedUsers).equal(typeof Array());
+    test('return user bu id',async () => {
+        const id=1;
+        sinon.stub(userModel, 'findOne').returns(usersObject);
+        const returnedUser = await userService.getUserById(id);
+        expect(returnedUser).equal(usersObject);
+    }),
+    
+    test('return type of users',async () => {
+        sinon.stub(userModel, 'find').returns(usersObject);
+        const returnedUsers = await userService.getAllUsers();
+        expect(typeof returnedUsers).equal(typeof Array());
+    });
 });
